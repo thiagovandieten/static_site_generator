6cecf4898e6e18a9d73a90660ce7815a8bee04bd
Thiago van Dieten <thiago@tvdieten.com>
2024-11-09 00:38:42 +0100

    add to_leafnode method for TextNode + unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
 from textnode import TextType, TextNode
+from leafnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -14,9 +15,33 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_not_eq_urls(self):
-        node = TextNode("I'm code!", TextType.LINK, "https://code.dev")
-        node2 = TextNode("I'm code!", TextType.LINK, "https://code.dev")
+        node = TextNode("I'm an url!", TextType.LINK, "https://code.dev")
+        node2 = TextNode("I'm an url!", TextType.LINK, "https://code.dev")
         self.assertEqual(node, node2)
+
+    def test_normal_text_to_node(self):
+        node = TextNode("I'm just text!", TextType.NORMAL)
+        self.assertEqual(node.to_leafNode(), LeafNode("I'm just text!"))
+
+    def test_bold_text_to_node(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        self.assertEqual(node.to_leafNode(), LeafNode("Bold text", "b"))
+
+    def test_italic_text_to_node(self):
+        node = TextNode("Italic text", TextType.ITALIC)
+        self.assertEqual(node.to_leafNode(), LeafNode("Italic text", "i"))
+
+    def test_code_text_to_node(self):
+        node = TextNode("Code text", TextType.CODE)
+        self.assertEqual(node.to_leafNode(), LeafNode("Code text", "code"))
+
+    def test_link_text_to_node(self):
+        node = TextNode("Link text", TextType.LINK, "https://example.com")
+        self.assertEqual(node.to_leafNode(), LeafNode("Link text", "a", {"href": "https://example.com"}))
+
+    def test_image_text_to_node(self):
+        node = TextNode("Image text", TextType.IMAGE, "https://example.com/image.png")
+        self.assertEqual(node.to_leafNode(), LeafNode("Image text", "img", {"src": "https://example.com/image.png"}))
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from leafnode import LeafNode
 
 class TextType(Enum):
     NORMAL = "normal"
@@ -18,7 +18,7 @@ class TextNode:
 
     def __eq__(self, other: object):
         if not isinstance(other, TextNode):
-            return NotImplemented
+            raise NotImplementedError
 
         if (
             self.text == other.text
@@ -31,3 +31,20 @@ class TextNode:
         
     def __repr__(self) -> str:
         return f"TextNode: ({self.text}, {self.text_type}, {self.url})"
+    
+    def to_leafNode(self):
+        match self.text_type:
+            case TextType.NORMAL:
+                return LeafNode(self.text)
+            case TextType.BOLD:
+                return LeafNode(self.text, "b")
+            case TextType.ITALIC:
+                return LeafNode(self.text, "i")
+            case TextType.CODE:
+                return LeafNode(self.text, "code")
+            case TextType.LINK:
+                return LeafNode(self.text, "a", {"href": self.url})
+            case TextType.IMAGE:
+                return LeafNode(self.text, "img", {"src": self.url})
+            case _:
+                raise ValueError("Invalid TextType")
