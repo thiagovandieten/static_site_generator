bf0d1c10fc685bc5452f554f91aa386b6dca4be1
Thiago van Dieten <thiago@tvdieten.com>
2024-11-14 23:50:39 +0100

    add more split methods for links and images

--- src/test_util.py
+++ src/test_util.py
@@ -2,30 +2,78 @@ import unittest
 from util import *
 from textnode import TextNode, TextType
 
+
 class test_util(unittest.TestCase):
 
     def test_split_notes_code(self):
         test_list = [
             TextNode("I already have a non-normal format", TextType.BOLD),
-            TextNode("I should be `code` but my type is Normal", TextType.NORMAL)
+            TextNode("I should be `code` but my type is Normal", TextType.NORMAL),
             # TextNode("I should be *italic* but my type is Normal", TextType.NORMAL)
         ]
         expected_list = [
             TextNode("I already have a non-normal format", TextType.BOLD),
             TextNode("I should be ", TextType.NORMAL),
             TextNode("code", TextType.CODE),
-            TextNode(" but my type is Normal", TextType.NORMAL)
-
+            TextNode(" but my type is Normal", TextType.NORMAL),
         ]
-        self.assertEqual(split_nodes_delimiter(test_list,'`', TextType.CODE), expected_list)
-    
+        self.assertEqual(
+            split_nodes_delimiter(test_list, "`", TextType.CODE), expected_list
+        )
+
     def test_extract_image(self):
         test_str = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-        expected_result = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"),("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        expected_result = [
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ]
         self.assertEqual(extract_markdown_images(test_str), expected_result)
 
     def test_extract_hyperlink(self):
         test_str = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-        expected_result = [("to boot dev", "https://www.boot.dev"),("to youtube", "https://www.youtube.com/@bootdotdev")]
+        expected_result = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev"),
+        ]
         self.assertEqual(extract_markdown_links(test_str), expected_result)
 
+    def test_split_nodes_images(self):
+        test_nodes = [
+            TextNode(
+                "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+                TextType.NORMAL,
+            )
+        ]
+        expected_nodes_list = [
+            TextNode("This is text with a ", TextType.NORMAL),
+            TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ]
+        nodes_list = split_nodes_images(test_nodes)
+        self.assertEqual(nodes_list, expected_nodes_list)
+
+    def test_split_nodes_links(self):
+        test_nodes = [
+            TextNode(
+                "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+                TextType.NORMAL,
+            )
+        ]
+        expected_nodes_list = [
+            TextNode("This is text with a link ", TextType.NORMAL),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode(
+                "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+            ),
+        ]
+        nodes_list = split_nodes_links(test_nodes)
+        self.assertEqual(nodes_list, expected_nodes_list)
+
+    def test_split_nodes_links_no_link(self):
+        test_nodes = [
+            TextNode("This is text with no link", TextType.NORMAL),
+        ]
+        with self.assertRaises(ValueError):
+            split_nodes_links(test_nodes)
--- src/util.py
+++ src/util.py
@@ -1,6 +1,6 @@
-# from enum import Enum
 from textnode import TextNode, TextType
 import re
+
 
 def split_nodes_delimiter(
     old_nodes: list[TextNode], delimiter, text_type: TextType
@@ -9,7 +9,7 @@ def split_nodes_delimiter(
     md_delimiters = {"`": TextType.CODE, "*": TextType.ITALIC, "**": TextType.BOLD}
 
     if delimiter not in md_delimiters.keys():
-        raise AttributeError("Not a valid markdown delimiter")
+        raise ValueError("Not a valid markdown delimiter")
 
     new_nodes: list[TextNode] = []
     for node in old_nodes:
@@ -32,26 +32,100 @@ def _generate_texttype_list(first_index, len_words, texttype):
     texttypes = []
     if first_index == 0:
         for i in range(len_words):
-            (
+            if i % 2 == 0:
                 texttypes.append(texttype)
-                if i % 2 == 0
-                else texttypes.append(TextType.NORMAL)
-            )
+            else:
+                texttypes.append(TextType.NORMAL)
     else:
         for i in range(len_words):
-            (
+            if i % 2 == 0:
                 texttypes.append(TextType.NORMAL)
-                if i % 2 == 0
-                else texttypes.append(texttype)
-            )
+            else:
+                texttypes.append(texttype)
 
     return texttypes
+
+
+def split_nodes_images(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+            continue
+
+        image_matches = extract_markdown_images(node.text)
+
+        if not image_matches:
+            raise ValueError("No image tag found")
+
+        remaining_text = node.text
+
+        for alt_text, url in image_matches:
+            remaining_text = _split_and_insert_image_or_hyperlink(
+                new_nodes, remaining_text, alt_text, url, TextType.IMAGE
+            )
+
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.NORMAL))
+
+    return new_nodes
+
+
+def split_nodes_links(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+            continue
+
+        link_matches = extract_markdown_links(node.text)
+
+        if not link_matches:
+            raise ValueError("No hyperlink tag found")
+
+        remaining_text = node.text
+
+        for alt_text, url in link_matches:
+            remaining_text = _split_and_insert_image_or_hyperlink(
+                new_nodes, remaining_text, alt_text, url, TextType.LINK
+            )
+
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.NORMAL))
+
+    return new_nodes
+
+
+def _split_and_insert_image_or_hyperlink(
+    new_nodes: list[TextNode],
+    remaining_text: str,
+    alt_text: str,
+    url: str,
+    texttype: TextType,
+):
+    if texttype == TextType.IMAGE:
+        markdown_format = f"![{alt_text}]({url})"
+    elif texttype == TextType.LINK:
+        markdown_format = f"[{alt_text}]({url})"
+
+    parts = remaining_text.split(markdown_format, 1)
+    if parts[0]:
+        new_nodes.append(TextNode(parts[0], TextType.NORMAL))
+    new_nodes.append(TextNode(alt_text, texttype, url))
+    remaining_text = parts[1]
+    return remaining_text
+
 
 def extract_markdown_images(text):
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
+
 def extract_markdown_links(text):
     return re.findall(r"[^!]\[(.*?)\]\((.*?)\)", text)
+
+
 # I didn't get to use this method but would still like to keep it incase.
 
 # def _find_delimiter_in_line(string: str, delimiter: str, offset: int = 0) -> list[int]:
